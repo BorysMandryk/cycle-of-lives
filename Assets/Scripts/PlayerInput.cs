f9e6ba4e7ee9c5d03ba820c2e1c40baa77df82ce
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2023-07-07 16:50:13 +0300

    Refactor player input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float MoveValue { get; private set; }
+    public bool JumpPressed { get; private set; }
+    public bool FreezePressed { get; private set; }
+
+    private Controls _controls;
+
+    private void OnEnable()
+    {
+        _controls = new Controls();
+
+        _controls.Player.Move.performed += SetMove;
+        _controls.Player.Move.canceled += SetMove;
+
+        _controls.Player.Jump.performed += SetJump;
+        _controls.Player.Jump.canceled += SetJump;
+
+        _controls.Player.Freeze.performed += SetFreeze;
+        _controls.Player.Freeze.canceled += SetFreeze;
+
+        _controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Player.Move.performed -= SetMove;
+        _controls.Player.Move.canceled -= SetMove;
+
+        _controls.Player.Jump.performed -= SetJump;
+        _controls.Player.Jump.canceled -= SetJump;
+
+        _controls.Player.Freeze.performed += SetFreeze;
+        _controls.Player.Freeze.canceled += SetFreeze;
+
+        _controls.Player.Disable();
+    }
+
+    private void SetMove(InputAction.CallbackContext context)
+    {
+        MoveValue = context.ReadValue<float>();
+    }
+
+    private void SetJump(InputAction.CallbackContext context)
+    {
+        JumpPressed = context.ReadValueAsButton();
+    }
+
+    private void SetFreeze(InputAction.CallbackContext context)
+    {
+        FreezePressed = context.ReadValueAsButton();
+    }
+}
