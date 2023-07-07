4530d8fc0c11ca783cbdb2ff474867680f910843
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2023-07-07 17:41:39 +0300

    Create FreezeTracker class

---
+++ Assets/Scripts/FreezeTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeTracker
+{
+    private Queue<GameObject> _freezes = new Queue<GameObject>();
+    private int _maxFreezeNum;
+
+    public int Count => _freezes.Count;
+
+    public FreezeTracker(int maxFreezeNum)
+    {
+        _maxFreezeNum = maxFreezeNum;
+    }
+
+    public void AddFreeze(GameObject gameObject)
+    {
+        if (Count > _maxFreezeNum)
+        {
+            DequeueFreeze();
+        }
+        _freezes.Enqueue(gameObject);
+    }
+
+    public GameObject DequeueFreeze()
+    {
+        return _freezes.Dequeue();
+    }
+}
