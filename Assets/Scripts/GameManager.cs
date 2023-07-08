8b3c254b803f0e567b312b99111ac13b0a8490a1
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2023-07-08 21:44:19 +0300

    Make GameManager a singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject _playerPrefab;
     private Vector2 _spawnPos;
 
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
     public void SpawnPlayer()
     {
         Instantiate(_playerPrefab, _spawnPos, Quaternion.identity);
