e4a69d223363d66b3c6fcf642170a463aaa3d676
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2023-07-10 02:07:58 +0300

    Change thruster freeze type behaviour

--- Assets/Scripts/Freeze Types/ThrusterFreezeType.cs
+++ Assets/Scripts/Freeze Types/ThrusterFreezeType.cs
@@ -7,6 +7,11 @@ public class ThrusterFreezeType : FreezeType
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Rigidbody2D rigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (rigidbody.velocity.y < -0.01f)
+        {
+            Debug.Log(rigidbody.velocity.ToString("F8"));
+            return;
+        }
         rigidbody.velocity = Vector2.zero;
         rigidbody.AddForce(Vector2.up * _thrustForce, ForceMode2D.Impulse);
     }
