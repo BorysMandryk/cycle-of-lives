311e57b4178cbe5c2cf4a5719c5500263aa4187b
BorysMandryk <55620815+BorysMandryk@users.noreply.github.com>
2023-08-02 17:43:39 +0300

    Stop player movement on level loader

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -16,10 +16,15 @@ public class LevelLoader : MonoBehaviour
         {
             return;
         }
-
         _triggered = true;
 
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        rb.velocity = Vector2.zero;
+
         // Варіант 1
+        // Проблема цього методу в тому, що він зберігає інформацію між сесіями,
+        // що в цій грі не потрібно
         PlayerPrefs.SetString("LastExitName", _exitName);
 
         // Варіант 2
